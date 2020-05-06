b5b9e0253a6fc3d30fbc232d308b58d96cd58c1f
Leonardo <leitee23@hotmail.com>
2020-05-05 22:54:45 -0300

    namespace changed

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace SchoolMngr.Service.Student.Controllers
+namespace SchoolMngr.Services.Academe.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace SchoolMngr.Service.Student
+namespace SchoolMngr.Services.Academe
 {
     public class Program
     {
