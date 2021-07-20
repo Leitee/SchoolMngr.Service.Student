742d5feb30d764e3b9d34db2d3cc731f88453140
Leonardo Moreno <leonardo.moreno.ext@vision-box.com>
2021-07-20 20:52:36 -0300

    start adding entities

---
+++ SchoolMngr.Services.Academe.Domain/Entities/Student.cs
@@ -0,0 +1,15 @@
+﻿/// <summary>
+/// 
+/// </summary>
+namespace SchoolMngr.Microservices.Academe.Domain
+{
+    using Pandora.NetStdLibrary.Base.DomainModel;
+    using System;
+
+    public class Student : EFEntity
+    {
+        public string Adress { get; set; }
+
+        public string Obs { get; set; }
+    }
+}
